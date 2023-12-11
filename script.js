08354a18aa0f2ae0e849f79eb46689f1d44ee5d5
sdum72 <sdum@hotmail.com>
2023-12-12 10:30:13 +1400

    updated js code

---
+++ script.js
@@ -0,0 +1,67 @@
+  // Instructs engine to load the HTML and CSS before running the JS
+  $(document).ready(function () {
+
+  var displayTimeEl = $('#display-time');
+
+  function displayTime() {
+    var rightNow = dayjs().format('dddd, DD MMMM YYYY, hh:mm:ss a');
+    displayTimeEl.text(rightNow);
+  }
+  displayTime()
+
+
+  // Assign saveBtn click listener for user input and get row id and save to local storage
+  $(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    // Save text in local storage
+    localStorage.setItem(time, text);
+  });
+
+
+
+  function hourTracker() {
+    // Get current number of hours.
+    var currentHour = dayjs().hour();
+  
+    // Loop over time blocks
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+  
+      // Check the time and add the classes for background indicators
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+  hourTracker();
+
+
+
+// Create a function to loop over time blocks to retrieve and display data from local storage
+function displayText() {
+  // Loop over time blocks
+  $(".time-block").each(function () {
+    // var blockHour = parseInt($(this).attr("id").split("-")[1]);
+    var blockHour = $(this).attr("id");
+    $(this).children(".description").val(localStorage.getItem(blockHour));
+  });
+}
+displayText();
+
+});
+
+    //Clear button function for clearing content and local storage
+    $("#clearFieldsBtn").click(function(event) {
+      event.preventDefault;
+      $("textArea").val("");
+      localStorage.clear();
+  });
