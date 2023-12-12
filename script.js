c4e1ea6f0bf8fcf80802e3a465be6430ad14f0e7
sdum72 <sdum@hotmail.com>
2023-12-12 11:31:52 +0000

    completed js code.

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-  // Instructs engine to load the HTML and CSS before running the JS
+  // Load the HTML and CSS before running the JS
   $(document).ready(function () {
 
-  var displayTimeEl = $('#display-time');
+  var displayTimeEl = $('#currentDay');
 
   function displayTime() {
     var rightNow = dayjs().format('dddd, DD MMMM YYYY, hh:mm:ss a');
     displayTimeEl.text(rightNow);
   }
   displayTime()
-
-
+ 
   // Assign saveBtn click listener for user input and get row id and save to local storage
   $(".saveBtn").on("click", function () {
     var text = $(this).siblings(".description").val();
@@ -19,45 +18,35 @@
     localStorage.setItem(time, text);
   });
 
-
-
-  function hourTracker() {
-    // Get current number of hours.
-    var currentHour = dayjs().hour();
-  
-    // Loop over time blocks
-    $(".time-block").each(function () {
-      var blockHour = parseInt($(this).attr("id").split("-")[1]);
-  
-      // Check the time and add the classes for background indicators
-      if (blockHour < currentHour) {
-        $(this).addClass("past");
-      } else if (blockHour === currentHour) {
-        $(this).removeClass("past");
-        $(this).addClass("present");
-      } else {
-        $(this).removeClass("past");
-        $(this).removeClass("present");
-        $(this).addClass("future");
-      }
-    });
-  }
-  hourTracker();
-
-
-
-// Create a function to loop over time blocks to retrieve and display data from local storage
+// function to loop over time blocks to retrieve and display data from local storage
 function displayText() {
-  // Loop over time blocks
+      // Get current number of hours.
+      var currentHour = dayjs().hour();
+      
+      // var blockHour = parseInt($(this).attr("id").split("-")[1]);
+    
+      // Loop over time blocks
+      $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+                 
+        // Check the time and add the classes for background indicators
+        if (blockHour < currentHour) {
+          $(this).addClass("past");
+        } else if (blockHour === currentHour) {
+          $(this).addClass("present");
+        } else {
+          $(this).addClass("future");
+        }
+       
+      });
+    
   $(".time-block").each(function () {
-    // var blockHour = parseInt($(this).attr("id").split("-")[1]);
     var blockHour = $(this).attr("id");
     $(this).children(".description").val(localStorage.getItem(blockHour));
   });
 }
 displayText();
 
-});
 
     //Clear button function for clearing content and local storage
     $("#clearFieldsBtn").click(function(event) {
@@ -65,3 +54,5 @@ displayText();
       $("textArea").val("");
       localStorage.clear();
   });
+
+});
